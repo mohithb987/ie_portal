63311e54a20ce60cf9d6478e4a8cf25fb2fd38bb
ashishreddy <ashishpatlolla.reddy46@gmail.com>
2020-08-06 21:39:32 +0530

    home is changed

--- home.php
+++ home.php
@@ -5,12 +5,7 @@
 <body>
 <p style="text-align: center; font-size:40px; color: brown; background-color: lightgrey;"> Shop for Electronic items here! </p>
 <p style="font-size: 25px;"> Categories: </p>
-<ul>
-	<li><a href="https://www.apple.com/in/iphone/?&cid=wwa-in-kwgo-iphone-slid-&mnid=sv0LhYgZi-dc_mtid_20925gyh65756_pcrid_431638207157_pgrid_105668579732_&mtid=20925gyh65756&aosid=p238&anonymizeip=set">Mobiles</a></li><br/>
-	<li><a href="https://www.myntra.com/watches">Watches</a></li><br/>
-	<li><a href="https://www.dell.com/en-in/shop/deals/desktop-deals?gacd=9684407-9004-5761040-0-0&dgc=ST&&gclid=CjwKCAjw_LL2BRAkEiwAv2Y3Sf7cvNo0o9FjP-kykvfg1CyG223b9nBQkJtzNkY2oqGiMBg9Zct8ZxoClUQQAvD_BwE&gclsrc=aw.ds">Laptops</a></li><br/>
-	<li><a href="https://www.flipkart.com/camera-clp-store">Cameras</a></li><br/>
-</ul>
+
 <footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
 </body>
 </html>
