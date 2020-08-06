981b9d7f515d96ec69cabb82b202d42500642a25
mohithb987 <mohithb987@gmail.com>
2020-08-06 22:46:43 +0530

    edited signup.html tag

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ function myFunction1(){
 		<form action="home.php" style="border-style: solid; border-width: 2px; text-align:center;margin-left: 550px;margin-right: 550px; padding: 40px 40px; ">
 		Enter password: <input type="text" name="pass"/><br/><br/>
 		<button type="submit" name="submit" id="submit" style="text-align: center; padding: 10px 10px;" onmouseenter="myFunction()" onmouseleave="myFunction1()">Login</button><br/><br/>
-		<a href="signup1.php">Click here to register</a>
+		<a href="signup1.php">Click here to register asbansfiafsdapo</a>
 		</form>
 
 </body>
