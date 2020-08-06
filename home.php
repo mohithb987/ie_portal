bb1477e42a315dbe1647b53df35c4ca40e66f3bb
mohithb987 <mohithb987@gmail.com>
2020-08-06 22:20:10 +0530

    Removed all footers

--- home.php
+++ home.php
@@ -6,16 +6,6 @@
 <p style="text-align: center; font-size:40px; color: brown; background-color: lightgrey;"> Shop for Electronic items here! </p>
 <p style="font-size: 25px;"> Categories: </p>
 
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-<footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
-
 
 <footer style="height: 100px; font-size: 30px; color: white;background-color: black; text-align: center;"> <p>Thank You!</p></footer>
 </body>
